faf32bb15a4f878b3b7f6579c13b1e14f47d953c
kendra24092007 <marroquin.l.kendra.loyola@gmail.com>
2025-03-07 11:38:05 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace EF_KendraM_B1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textnumero1 = new System.Windows.Forms.TextBox();
+            this.textnumero2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(180, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(247, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculador de números";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label2.Location = new System.Drawing.Point(180, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(249, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "- Ingresa dos números -";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label3.Location = new System.Drawing.Point(120, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 27);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Numero 1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label4.Location = new System.Drawing.Point(410, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 27);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Numero 2";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textnumero1
+            // 
+            this.textnumero1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.textnumero1.Location = new System.Drawing.Point(116, 179);
+            this.textnumero1.Name = "textnumero1";
+            this.textnumero1.Size = new System.Drawing.Size(114, 33);
+            this.textnumero1.TabIndex = 4;
+            this.textnumero1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textnumero2
+            // 
+            this.textnumero2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.textnumero2.Location = new System.Drawing.Point(403, 179);
+            this.textnumero2.Name = "textnumero2";
+            this.textnumero2.Size = new System.Drawing.Size(127, 33);
+            this.textnumero2.TabIndex = 5;
+            this.textnumero2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button1.Location = new System.Drawing.Point(195, 265);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 48);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(634, 390);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textnumero2);
+            this.Controls.Add(this.textnumero1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Kristen ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textnumero1;
+        private System.Windows.Forms.TextBox textnumero2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace EF_KendraM_B1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((int.TryParse(textnumero1.Text, out int numero1))&&(int.TryParse(textnumero2.Text, out int numero2)))
+            {
+                if (numero1 < numero2 )
+                {
+                    MessageBox.Show("El primer numero es menor a  " + numero2);
+                }
+                else if (numero1 == numero2)
+                {
+                    MessageBox.Show("El primer numero es igual a  " + numero2);
+                }
+                else
+                {
+                    MessageBox.Show("El primer es mayor a  " + numero2);
+                }
+                
+            }
+           
+              
+            else
+            {
+                MessageBox.Show("Ingrese un valor valido");
+            }
+        }
+    }
+}
